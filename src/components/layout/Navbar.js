5b5db38b29394e33999a0b042214d8a1a0eb8a1f
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-09-26 20:49:40 -0400

    initial commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-5">
+      <span className="navbar-brand mb-01 mx-auto">LyricFinder</span>
+    </nav>
+  );
+};
+
+export default Navbar;
